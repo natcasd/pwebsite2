05442080e7925ac4dd77f594b98541fabd06a7a2
natcasd <nathan_depiero@brown.edu>
2024-12-30 15:53:39 -0500

    favicon

--- src/app/components/Contact.tsx
+++ src/app/components/Contact.tsx
@@ -2,14 +2,11 @@ import Contact from "../components/contactform";
 
 export default function ContactPage() {
   return (
-    <div id="contact">
+    <div id="contact" className="flex justify-top items-center my-10">
       
-        <div className="flex justify-top items-center h-screen">
           <div className="w-full">
             <div className="p-5 sm:p-10 border border-gray-400 rounded-lg bg-gray-200 bg-opacity-50">
-              <Contact />
-            </div>
-          </div>
+              <Contact />      </div>
         </div>
         
 </div>  );
