4d41b48e49413b4a7c6378b9b52fddf9657cdc0f
natcasd <nathan_depiero@brown.edu>
2025-01-29 16:38:16 -0500

    testing greeting

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -1,8 +1,16 @@
+"use client"
 import Image from "next/image";
 import profile from "../../../public/images/profile.png"; // Adjust the path as needed
 import { getGreeting } from "../utils/getGreeting";
+//import { useEffect, useState } from 'react';
 
 export default function Hero() {
+  // const [greeting, setGreeting] = useState('');
+
+  // // Update the greeting when the component mounts
+  // useEffect(() => {
+  //   setGreeting(getGreeting());
+  // }, []);
   const greeting = getGreeting()
   return (
       <div id="about" className="scroll-mt-20 flex justify-center items-center h-full w-full space-x-3">
@@ -13,7 +21,7 @@ export default function Hero() {
           className="w-1/3 h-1/3 md:w-1/4 md:h-1/4 object-cover object-center rounded-lg"
         />
         <div className="flex flex-col leading-none">
-          <h1 className="text-[6vw] sm:text-[4vw] italic">{greeting}!</h1>
+          <h1 className="text-[6vw] sm:text-[4vw] italic">{greeting}</h1>
           <h1 className="text-[10vw] lg:text-[7vw] sm:text-[7vw] xs:text-[5vw] font-bold">
             I&apos;m <span className="underline">Nathan</span>.
           </h1>
--- src/app/utils/getGreeting.ts
+++ src/app/utils/getGreeting.ts
@@ -1,14 +1,16 @@
 import dayjs from 'dayjs'
 
 function getGreeting(): string {
-  const hour = dayjs().hour()
-  if (hour >= 4 && hour <= 11) {
-    return 'Good morning'
-  }
-  if (hour >= 11 && hour <= 17) {
-    return 'Good afternoon'
-  }
-  return 'Good evening'
+  // const hour = dayjs().hour()
+  // if (hour >= 4 && hour <= 11) {
+  //   return 'Good morning!'
+  // }
+  // if (hour >= 11 && hour <= 17) {
+  //   return 'Good afternoon!'
+  // }
+  // return 'Good evening!'
+  const fullDateTime = dayjs().format('YYYY-MM-DD HH:mm:ss');
+  return fullDateTime
 }
 
 export { getGreeting }
